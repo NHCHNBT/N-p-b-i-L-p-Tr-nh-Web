847d52b384b3b9cc26595c1199bc8871429dc11f
NHCHNBT <nxichthanh5@gmail.com>
2025-09-29 13:17:17 +0700

    24DH111693-lab1b-LTW

---
+++ 24DH111693-lab1b-LTW/Controllers/ppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24DH111693_lab1b_LTW.Controllers
+{
+    public class ppController : Controller
+    {
+        // GET: pp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
