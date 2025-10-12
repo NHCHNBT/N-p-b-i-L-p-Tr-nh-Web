1199f7c0a1b1b7ace4cb0a8ca74cdf3dae5e79a7
NHCHNBT <nxichthanh5@gmail.com>
2025-10-13 00:46:48 +0700

    Nhan Xích Thành -KTraFE(1)

---
+++ Nhan Xích Thành -KTraFE(1)/Controllers/MasterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhan_Xích_Thành__KTraFE_1_.Controllers
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Phone()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nhan Xích Thành -KTraFE(1)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nhan_Xích_Thành__KTraFE_1_.MvcApplication" Language="C#" %>
