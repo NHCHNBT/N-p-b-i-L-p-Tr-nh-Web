1f67e6692e84e39bb47f0d5f17e5c3104a06802d
NHCHNBT <nxichthanh5@gmail.com>
2025-10-27 00:02:41 +0700

    Nhan Xích Thành-Đồ án(BE)-LTW

---
+++ Nhan Xích Thành-Đồ án(BE)-LTW/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Nhan_Xích_Thành_Đồ_án_BE__LTW
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Nhan Xích Thành-Đồ án(BE)-LTW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nhan_Xích_Thành_Đồ_án_BE__LTW.MvcApplication" Language="C#" %>
